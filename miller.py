2306853c29e8160578bbc59c0749e83a08b075ec
Ava Dean <ava.dean@york.ac.uk>
2025-06-02 18:03:59 +0100

    ruff check changes

--- miller.py
+++ miller.py
@@ -1,15 +1,14 @@
-from numpy import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 A = 2.2
 kappa = 1.5
 delta = 0.3
 R0 = 2.5
-theta = linspace(0, 2 * pi)
+theta = np.linspace(0, 2 * np.pi)
 r = R0 / A
-R_s = R0 + r * cos(theta + (arcsin(delta) * sin(theta)))
-Z_s = kappa * r * sin(theta)
-
-import matplotlib.pyplot as plt
+R_s = R0 + r * np.cos(theta + (np.arcsin(delta) * np.sin(theta)))
+Z_s = kappa * r * np.sin(theta)
 
 plt.plot(R_s, Z_s)
 plt.axis("equal")
