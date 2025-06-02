41303edb586a584e0349ae4edb0fe526f724e735
Ava Dean <ava.dean@york.ac.uk>
2025-06-02 18:51:38 +0100

    1_reusable_module work

--- miller.py
+++ miller.py
@@ -1,17 +1,72 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-A = 2.2
-kappa = 1.5
-delta = 0.3
-R0 = 2.5
-theta = np.linspace(0, 2 * np.pi)
-r = R0 / A
-R_s = R0 + r * np.cos(theta + (np.arcsin(delta) * np.sin(theta)))
-Z_s = kappa * r * np.sin(theta)
 
-plt.plot(R_s, Z_s)
-plt.axis("equal")
-plt.xlabel("R [m]")
-plt.ylabel("Z [m]")
-plt.savefig("../miller.png")
+def flux_surface(R0, A, kappa, delta, theta):
+    """
+    Creates a Miller flux surface
+
+    Parameters
+    ----------
+    R0 : float
+        Major radius of the magnetic axis
+    A : float
+        Aspect ratio
+    kappa : float
+        Elongation
+    delta : float
+        Triangularity
+    theta : 1D np.ndarray
+        Geometric poloidal angles
+
+    Returns
+    -------
+    R_s : 1D np.ndarray
+        Major radius of the flux surface
+    Z_s : 1D np.ndarray
+        Vertical coordinate of the flux surface
+    """
+
+    r = R0 / A
+
+    return R0 + r * np.cos(
+        theta + (np.arcsin(delta) * np.sin(theta))
+    ), kappa * r * np.sin(theta)
+
+
+def plot_surface(R_s, Z_s, savefig=True):
+    """
+    Creates a Miller flux surface
+
+    Parameters
+    ----------
+    R_s : 1D np.ndarray
+        Major radius of the flux surface
+    Z_s : 1D np.ndarray
+        Vertical coordinate of the flux surface
+    savefig : bool, default=True
+
+    Returns
+    -------
+    None
+    """
+
+    plt.plot(R_s, Z_s)
+    plt.axis("equal")
+    plt.xlabel("R [m]")
+    plt.ylabel("Z [m]")
+
+    if savefig:
+        plt.savefig("./miller.png")
+
+
+def main():
+    R_s, Z_s = flux_surface(
+        R0=2.5, A=2.2, kappa=1.5, delta=0.3, theta=np.linspace(0, 2 * np.pi)
+    )
+
+    plot_surface(R_s, Z_s)
+
+
+if __name__ == "__main__":
+    main()
